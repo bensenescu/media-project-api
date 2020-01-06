64f83fe03ac91965d3a70556ec851de9a926b18e
Ben Senescu <ben@Bens-MacBook-Pro.local>
2020-01-06 16:19:25 -0500

    working routes

--- index.js
+++ index.js
@@ -1,34 +1,23 @@
-const serverless = require('serverless-http');
 const express = require('express');
 const AWS = require('aws-sdk');
+const bodyParser = require('body-parser');
 
 const credentials = new AWS.SharedIniFileCredentials();
 AWS.config.credentials = credentials;
+AWS.config.region = 'us-east-1';
 
 const app = express();
-const mediaController = require('./controllers/mediaController');
+const mediaRouter = require('./routes/mediaRouter');
+
+app.use(bodyParser.json({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/hello', (req, res) => {
-  res.send('hello');
-});
+app.use('/api', mediaRouter);
 
-// act on all media in the database
-app.route('/api/media')
-// QUERY PARAMETERS
-// username: gives you all media made by a user
-// quantity: quantity to get back, default most recent 20
-  .get(mediaController.getAllMedia)
-  .post(mediaController.createMedia);
-
-// act on an individual piece of media,
-app.route('/api/media/:mediaId')
-  .get(mediaController.getMedia)
-  .post(mediaController.updateMedia)
-  .delete(mediaController.deleteMedia);
-
+// eslint-disable-next-line no-console
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
-module.exports.handler = serverless(app);
+
+module.exports = app;
